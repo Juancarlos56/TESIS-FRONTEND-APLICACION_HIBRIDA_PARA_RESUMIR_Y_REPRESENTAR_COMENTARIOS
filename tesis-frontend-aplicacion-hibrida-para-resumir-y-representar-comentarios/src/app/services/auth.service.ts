58573375c4ba455bacfbf524cddd7f570e819616
Katherine <51839660+katerinbarrera21@users.noreply.github.com>
2022-07-03 16:04:57 -0500

    correcion funcion getUID

--- tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/services/auth.service.ts
+++ tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/services/auth.service.ts
@@ -69,15 +69,6 @@ export class AuthService {
     }  
   }
 
-  async getUid(){
-    const user = await this.auth.currentUser
-    if (user){
-      return user.uid;
-    } else {
-      return null;
-    }  
-  }
-
   
   
 
