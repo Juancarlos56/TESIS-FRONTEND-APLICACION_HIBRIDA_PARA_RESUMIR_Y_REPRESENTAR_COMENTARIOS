2d350abe85d55ebfbc841e73eb4179888c69bdd0
Katherine <51839660+katerinbarrera21@users.noreply.github.com>
2022-07-05 22:09:58 -0500

    update

--- tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/usuario/usuario-lista-comentarios/usuario-lista-comentarios.page.ts
+++ tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/usuario/usuario-lista-comentarios/usuario-lista-comentarios.page.ts
@@ -24,6 +24,7 @@ export class UsuarioListaComentariosPage implements OnInit {
 
   }
 
+
   listarComentario(){
     const usuarioemail = this.listaService.getUsuarioEmail();
     console.log(usuarioemail)
