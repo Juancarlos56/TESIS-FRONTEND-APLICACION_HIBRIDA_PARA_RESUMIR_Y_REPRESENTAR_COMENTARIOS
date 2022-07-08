676b8e97ce413f60d10b2139ed71d9d95563bcbc
Juan Carlos Barrera B <51795550+Juancarlos56@users.noreply.github.com>
2022-07-08 15:31:21 -0500

    libreria graficos

--- tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/admin/adminkeywords/adminkeywords.page.ts
+++ tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/admin/adminkeywords/adminkeywords.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-adminkeywords',
@@ -8,15 +9,58 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 })
 export class AdminkeywordsPage implements OnInit {
 
+  public pruebaChart : Chart; 
+
   constructor( private serviceStorage : FirestoreService) { }
 
   ngOnInit() {
     this.mostrarImagen();
+    
   }
-
+  ngAfterViewInit(){
+    this.graficaPrueba()
+  }
   mostrarImagen(){
     console.log(this.serviceStorage.traerImagenesStorage())
 
+  }
+  
+  graficaPrueba(){
+    
+    this.pruebaChart  = new Chart('myChart', {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+    });
   }
 
 }
