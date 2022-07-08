295ec2bebf96e59155f1db45434d745eb1fb590f
Katherine <51839660+katerinbarrera21@users.noreply.github.com>
2022-07-08 00:26:02 -0500

    graficas

--- tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/admin/admintop10/admintop10.page.ts
+++ tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/admin/admintop10/admintop10.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-admintop10',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Admintop10Page implements OnInit {
 
+  private pruebaChart : Chart;
+  private milinearChart : Chart;
   constructor() { }
 
   ngOnInit() {
+    this.generarChart(); 
+    this.graficaPastel();
+
   }
+
+  generarChart(){
+
+    this.pruebaChart  = new Chart('myChart', {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+    });
+
+}
+
+graficaPastel(){
+  this.milinearChart  = new Chart('myChart1', { 
+    type: 'doughnut',
+    data : {
+      labels: ['Red','Blue','Yellow'],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+  }
+  });
+}
 
 }
--- tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/app.module.ts
+++ tesis-frontend-aplicacion-hibrida-para-resumir-y-representar-comentarios/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth, GoogleAuthProvider } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
